3640b1a606d395ee525cacbffe6c436bb081b3b3
Antonio <joseantonio.tancoo@gmail.com>
2024-07-01 23:01:55 +0200

    Second commit

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,16 @@
-import enIndex from '@i18n/en/index.json'
-import deIndex from '@i18n/de/index.json'
-import frIndex from '@i18n/fr/index.json'
+import enIndex from '@i18n/en/index.json';
+import deIndex from '@i18n/de/index.json';
+import frIndex from '@i18n/fr/index.json';
+
+import enLegal from '@i18n/en/legal.json';
+import deLegal from '@i18n/de/legal.json';
+import frLegal from '@i18n/fr/legal.json';
 
 
 const supportedLanguages = ["en", "de", "fr"];
 const defaultLanguage = "en";
 
-type I18nSection = "index";
+type I18nSection = "index" | "legal";
 type Translations = {
     [key in I18nSection]: any;
 };
@@ -14,12 +18,15 @@ type Translations = {
 const translations: Record<string, Translations> = {
     en: {
         index: enIndex,
+        legal: enLegal
     },
     de: {
         index: deIndex,
+        legal: deLegal
     },
     fr: {
         index: frIndex,
+        legal: frLegal
     }
 };
 
